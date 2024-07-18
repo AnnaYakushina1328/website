5c9804e27d90b6e97164a9d700cfff3ffbd51459
AnnaYakushina1328 <121216353+AnnaYakushina1328@users.noreply.github.com>
2024-07-18 13:23:10 +0300

    подправила detail.html в shop

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,6 @@
 from .models import Category, Product
 from django.shortcuts import render, get_object_or_404
+from myshop.cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -27,3 +28,12 @@ def product_detail(request, slug):
 
 def home(request):
     return render(request, 'shop/base.html')
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
