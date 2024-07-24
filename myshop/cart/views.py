bac187c539ab2f1898f319672208af99826fb158
AnnaYakushina1328 <121216353+AnnaYakushina1328@users.noreply.github.com>
2024-07-24 09:05:21 +0300

    всё готово, только сломалось суммирование в корзине

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -12,9 +12,7 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product,
-                 quantity=cd['quantity'],
-                 update_quantity=cd['update'])
+        cart.add(product=product, quantity=cd['количество'], update_quantity=cd['изменить'])
     return redirect('cart:cart_detail')
 
 
--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
+    path('shop/', include('shop.urls')),
+    path('', RedirectView.as_view(url='shop')),
 ]
 
 if settings.DEBUG:
